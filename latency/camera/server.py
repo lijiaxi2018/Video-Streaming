83fe062d449b7aec652ce79ca8b6c38fccc04830
Jiaxi Li <jiaxili@MacBook-14.local>
2024-07-15 00:33:16 -0500

    feature: latency energy

--- latency/camera/server.py
+++ latency/camera/server.py
@@ -5,8 +5,8 @@ import json
 
 HOST = 'localhost'
 PORT = 65432
-RECEIVE_DIRECTORY = './temp/TF_ETE_S_4K_cropped'
-LOG_FILE = './temp/TF_ETE_S_4K_cropped.json'
+RECEIVE_DIRECTORY = './temp/server'
+LOG_FILE = './temp/server.json'
 
 def save_image(image_data, save_path):
     with open(save_path, 'wb') as f:
@@ -62,6 +62,8 @@ def start_server(result_dir, host='localhost', port=65432):
     except Exception as e:
         print(f'Error: {e}')
     finally:
+        LOG_DIR = os.path.dirname(LOG_FILE)
+        os.makedirs(LOG_DIR, exist_ok=True)  # Ensure the directory exists
         with open(LOG_FILE, 'w') as f:
             json.dump(log, f, indent=4)
         print(f'Saved log to {LOG_FILE}')
